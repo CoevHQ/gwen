b4e3d613d9514852445eb47581d6f36448148603
CoEvolution Labs <jenerestain@gmail.com>
2016-02-25 16:23:34 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,103 +3,103 @@ var flightPrice = function(){
   var to = document.getElementById('place2').value
   var price
   if(from=="France"&&to=="New York"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="France"&&to=="Australia"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="France"&&to=="Hawaii"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="France"&&to=="France"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="France"&&to=="Japan"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="New York"&&to=="New York"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="New York"&&to=="France"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="New York"&&to=="Hawaii"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="New York"&&to=="Australia"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="New York"&&to=="Japan"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
     else if(from=="Australia"&&to=="New York"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Australia"&&to=="France"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Australia"&&to=="Hawaii"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Australia"&&to=="Australia"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Australia"&&to=="Japan"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Hawaii"&&to=="New York"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Hawaii"&&to=="France"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Hawaii"&&to=="Hawaii"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Hawaii"&&to=="Australia"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Hawaii"&&to=="Japan"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
 else if(from=="Japan"&&to=="New York"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Japan"&&to=="France"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Japan"&&to=="Hawaii"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Japan"&&to=="Australia"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
   else if(from=="Japan"&&to=="Japan"){
-    price = "0 Pesos"
+    price = 0
     return price
   }
 }
@@ -110,5 +110,5 @@ var alertPrice = function(){
   var date=document.getElementById('date').value
   var date2=document.getElementById('date2').value
   var price = flightPrice()
-  alert("Hello " + name + ", the price for your destination from " + place1 + " to " + place2 + " and back is " + price*2 + " at " + date + " to " + date2 )
+  alert("Hello " + name + ", the price for your destination from " + place1 + " to " + place2 + " and back is " + price*2 + " Pesos" + " at " + date + " to " + date2 )
 }
